61bdb7dad547892d4d94a06c0082b7e771f12bc1
adolfo0620 <adolfo0620@gmail.com>
2015-01-09 16:29:43 -0500

    added a model for Query

--- project/users/models.py
+++ project/users/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Query(models.Model):
+	created_at = models.DateField(auto_now_add=True)
+	query_string = models.CharField (max_length=500)
+	negative_score = models.IntegerField()
+	positive_score = models.IntegerField()
+	user = models.ForeignKey( User )
+	media_platform = models.CharField(max_length=250)
