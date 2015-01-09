290566f24184aa1351f5e8503504f520ad03ed3d
adolfo0620 <adolfo0620@gmail.com>
2015-01-09 16:43:53 -0500

    moved user to User

--- project/twit/models.py
+++ project/twit/models.py
@@ -1,2 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class twittaccess(models.Model):
+	token = models.CharField(max_length=200)
+    secret = models.CharField(max_length=200)
+    user = models.ForeignKey( User )
