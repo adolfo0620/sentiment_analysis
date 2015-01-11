78842bb2ab956e3ddc746f1f732ccf8e7b3b023b
adolfo0620 <adolfo0620@gmail.com>
2015-01-10 21:17:12 -0500

    moved logic view to api.py

---
+++ project/sa_api/api.py
@@ -0,0 +1,17 @@
+
+class Score():
+    def __init__(self, string):
+        self.positive = ['love','loved','like','liked','awesome','amazing','good','great','excellent', 'nice', 'sweet']
+        self.negative = ['hate','hated','dislike','disliked','awful','terrible','bad','painful','worst', 'disgraceful', 'horrible']
+        self.pos = 0
+        self.neg = 0
+        self.text = string
+
+    def eval(self):
+        for result in self.text['statuses']:
+            for word in self.positive:
+                if word in result['text']:
+                    self.pos += 1
+            for word in self.negative:
+                if word in result['text']:
+                    self.neg += 1
