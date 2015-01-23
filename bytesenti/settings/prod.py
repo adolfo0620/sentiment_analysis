dde0419710f454ad59669d1aeea14a41d717f14b
Benjamin Himley <benjaminhimley85@gmail.com>
2015-01-23 12:35:13 -0500

    adding DATABASES

--- bytesenti/settings/prod.py
+++ bytesenti/settings/prod.py
@@ -62,6 +62,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
